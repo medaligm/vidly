4006d46d83ec2db1707c703af4f157ef7fbd068a
medali <gmaghalgaouimohamedali@gmail.com>
2019-11-15 10:52:20 +0100

    rentals routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
+const rentals = require('./routes/rentals');
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
