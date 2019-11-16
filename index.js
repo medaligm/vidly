7d96371999c01b4aaff2bdce09746ce0f56f17a7
medali <gmaghalgaouimohamedali@gmail.com>
2019-11-16 18:20:18 +0100

    storing secrets in envvironment variables with npm config

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const genres = require("./routes/genres");
@@ -9,6 +10,11 @@ const auth = require("./routes/auth");
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
+  process.exit(1);
+}
 
 mongoose
   .connect("mongodb://localhost/myVidly", {
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
-const jwt = require('jsonwebtoken')
+const config = require("config");
+const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
@@ -17,7 +18,7 @@ router.post("/", async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("invalid email or password.");
 
-  const token = jwt.sign({_id: user._id}, 'jwtPrivatekey')
+  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
   res.send(token);
 });
 
