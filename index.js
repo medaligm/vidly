d57a06b5434fc169f54849110eed1edb1efca863
medali <gmaghalgaouimohamedali@gmail.com>
2019-11-16 17:07:57 +0100

    Authenticating users with json web token

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
+const auth = require("./routes/auth");
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
@@ -24,6 +25,7 @@ app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
