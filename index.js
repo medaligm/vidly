bc55f35953355f271de7bff1dc79f20aec28acaf
medali <gmaghalgaouimohamedali@gmail.com>
2019-11-16 11:16:22 +0100

    create user API

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const Joi = require('joi')
+const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
-const rentals = require('./routes/rentals');
+const rentals = require("./routes/rentals");
+const users = require("./routes/users");
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
 mongoose
-  .connect("mongodb://localhost/myVidly")
+  .connect("mongodb://localhost/myVidly", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log("Connected to MongoDB..."))
   .catch(err => console.error("could not connect to MongoDB..."));
 
@@ -17,7 +22,8 @@ app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
-app.use('/api/rentals', rentals);
+app.use("/api/rentals", rentals);
+app.use("/api/users", users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
