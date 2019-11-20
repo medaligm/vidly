3ed92a7989158180220be8fb354f84605ad9ec1b
medali <gmaghalgaouimohamedali@gmail.com>
2019-11-20 15:07:14 +0100

    protecting routes

--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,5 @@
-const {Genre, validate} = require('../models/genre')
+const auth = require("../middleware/auth");
+const { Genre, validate } = require("../models/genre");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
@@ -8,7 +9,7 @@ router.get("/", async (req, res) => {
   res.send(genres);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   let genre = new Genre({ name: req.body.name });
@@ -42,6 +43,5 @@ router.get("/:id", async (req, res) => {
     return res.status(404).send("The genre with the given id was not found.");
   res.send(genre);
 });
-
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const auth = require("../middleware/auth");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const bcrypt = require("bcrypt");
@@ -6,6 +7,11 @@ const { User, validate } = require("../models/user");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
+
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
@@ -19,7 +25,7 @@ router.post("/", async (req, res) => {
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  const token = user.generateAuthToken()
+  const token = user.generateAuthToken();
   res.header("x-auth-token", token).send(_.pick(user, ["id", "name", "email"]));
 });
 
